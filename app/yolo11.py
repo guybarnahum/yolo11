ed1c7b89a8a3a16073af545fdde24486d1dbf8ef
guybarnahum <guybarnahum@users.noreply.github.com>
2025-02-28 10:27:05 -0700

    Provide reasonable defaults to process_one_frame

--- app/yolo11.py
+++ app/yolo11.py
@@ -5,7 +5,7 @@ from features.person.inspect import should_inspect as should_inspect_person
 from sahi.predict import get_sliced_prediction
 
 from trackers.deepsort.tracker import track as deepsort_track
-from utils import print_detections, flatten_results
+from utils import print_detections, flatten_results, cuda_device
 
 frames_to_debug = None # [1,2,3,4,5] 
 
@@ -19,23 +19,16 @@ def should_inspect( detection ):
     return False
 
 
-def process_one_frame( frame, detect_model, tile_model, tracker, tile, conf, frame_number = None, device='cpu' ):
+def process_one_frame( frame, detect_model, tile_model=None, tracker=None, tile=None, conf=None, frame_number = None, device=None ):
 
     global frames_to_debug
 
-    # 0: "person"
-    # 1: "bicycle"
-    # 2: "car"
-    # 3: "motorcycle"
-    # 4: "airplane"
-    # 5: "bus"
-    # 6: "train"
-    # 7: "truck"
-    # 8: "boat"
+    if not tracker  : tracker="botsort.yaml"
+    if not conf     : conf = 0.3
+    if not device   : device = cuda_device()
 
     class_codes = [0,1,2,3,4,5,6,7,8]
-    if not conf: conf = 0.3
-
+   
     if tile:
          # Get sliced predictions
         results = get_sliced_prediction( image=frame, detection_model=tile_model,
